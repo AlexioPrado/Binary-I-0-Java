ce2382491a38a48e24e250173e18513bdd800565
Marcus Prado <mgprado1@my.waketech.edu>
2026-01-28 18:23:27 +0000

    completed generics

--- Generics/generics.java
+++ Generics/generics.java
@@ -1,34 +1,22 @@
-public class GenericStack<E> {
-    private java.util.ArrayList<E> list = new java.util.ArrayList<>();
 
-    public int getSize() {
-        return list.size();
-    }
-
-    public E peek() {
-        return list.get(getSize() - 1);
-    }
-
-    public void push(E o) {
-        list.add(o);
-    }
-
-    public E pop() {
-        E o = list.get(getSize() - 1);
-        list.remove(getSize() - 1);
-        return o;
-    }
-
-    public boolean isEmpty() {
-        return list.isEmpty();
-    }
-
-    @Override public String toString() {
-        return "stack: " + list.toString();
-    }
-
-}
+import java.util.*;
 
 public class generics {
-    
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        GenericStack<String> stack = new GenericStack<>();
+
+        System.out.println("Enter five strings: ");
+        for (int i = 1; i<=5; i++){
+            stack.push(input.nextLine());
+        }
+
+        System.out.println("Outputting String in Reverse Order: ");
+        while (!stack.isEmpty()){
+            System.out.println(stack.pop());
+        }
+
+
+        input.close();
+    }
 }
